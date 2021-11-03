ac65004e809b6e6f38969e15d58b3954d2e0dbb3
WhatTheAncient <whattheanc1ent@yandex.ru>
2021-11-03 17:11:46 +0300

    3 task done

--- 3.rb
+++ 3.rb
@@ -14,7 +14,7 @@ i = 0
 code = 0
 File.open("data/3.txt", 'r').each_line do |line|
   numbers << line.split.map(&:to_i)
-  code += (numbers[i].max - numbers[i].min)
+  code += numbers[i].max - numbers[i].min
   i += 1
 end
 puts code
